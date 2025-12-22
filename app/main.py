8b648be0e2377174318aac77b3027eaee65635df
Vic <victorsalazar.01.vv@gmail.com>
2025-12-22 14:28:32 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,8 +5,12 @@ def main():
     while True:
         sys.stdout.write("$ ")
         
-        command = input()
-        print(f"{command}: command not found")
+        command = input().strip()
+        if command == "exit 0":
+            sys.exit(0)
+        
+        sys.stdout.write(f"{command}: command not found\n")
+
 
 
 if __name__ == "__main__":
