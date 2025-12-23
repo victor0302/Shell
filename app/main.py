b35210c84c9d31b92c08c66b7cea6e9367bf94bd
Vic <victorsalazar.01.vv@gmail.com>
2025-12-22 21:25:14 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -10,15 +11,31 @@ def main():
         command = input().strip()
         if command == "exit":
             sys.exit(0)
+
         elif command.startswith("type"):
             message = command [5:]
             if message in builtin:
                 print(f"{message} is a shell builtin")
             else:
-                print(f"{message}: not found")
+                env_path = os.environ.get("PATH")
+                paths = env_path.split(os.pathsep)
+                found = False
+
+                for path in paths:
+                    full_address = os.path.join(path,message)
+                    
+                    if os.access(full_address, os.X_OK):
+                        print(f"{message} is {full_address}")
+                        found = True
+                        break
+                if not found:
+                    print(f"{message}: not found")
+
+
         elif command.startswith("echo "):
             message = command[5:]
             print(message)
+
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
