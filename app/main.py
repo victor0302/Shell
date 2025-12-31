7dcd782d3c9173671d3d5078de6c6407a4bab777
Vic <victorsalazar.01.vv@gmail.com>
2025-12-31 00:33:21 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 import sys
 import os
+import subprocess
 
+def get_path(cmd):
+    env_path = os.environ.get("PATH", "")
+    
+    paths = env_path.split(os.pathsep)
+    
+    for path in paths:
+        full_path = os.path.join(path, cmd)
+        
+        if os.access(full_path, os.X_OK):
+            return full_path
+            
+    return None
 
 def main():
-    builtin = {"echo","exit","type"}
+    builtin = {"echo", "exit", "type"}
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
@@ -13,33 +26,30 @@ def main():
             sys.exit(0)
 
         elif command.startswith("type"):
-            message = command [5:]
+            message = command[5:]
             if message in builtin:
                 print(f"{message} is a shell builtin")
             else:
-                env_path = os.environ.get("PATH")
-                paths = env_path.split(os.pathsep)
-                found = False
-
-                for path in paths:
-                    full_address = os.path.join(path,message)
-                    
-                    if os.access(full_address, os.X_OK):
-                        print(f"{message} is {full_address}")
-                        found = True
-                        break
-                if not found:
+                path = get_path(message)
+                if path:
+                    print(f"{message} is {path}")
+                else:
                     print(f"{message}: not found")
-
 
         elif command.startswith("echo "):
             message = command[5:]
             print(message)
 
         else:
-            sys.stdout.write(f"{command}: command not found\n")
-
-
+            args = command.split()
+            program_name = args[0]
+            
+            path = get_path(program_name)
+            
+            if path:
+                subprocess.run(args, executable=path)
+            else:
+                sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
